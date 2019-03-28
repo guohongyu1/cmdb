a8398dccd7757ff06d4509bc89979f51c1033dc6
ghy <594820543@qq.com>
2019-03-28 10:26:56 +0800

    update

--- AutoCmdb/web/views/asset.py
+++ AutoCmdb/web/views/asset.py
@@ -3,7 +3,7 @@
 from django.views import View
 from django.shortcuts import render
 from django.http import JsonResponse
-
+from repository import models
 from web.service import asset
 
 
@@ -31,8 +31,27 @@ class AssetDetailView(View):
     def get(self, request, device_type_id, asset_nid):
         response = asset.Asset.assets_detail(device_type_id, asset_nid)
         return render(request, 'asset_detail.html', {'response': response, 'device_type_id': device_type_id})
-
-
+from django.forms import ModelForm
+class AssetList(ModelForm):
+  class Meta:
+    model = models.Asset #对应的Model中的类
+    fields = "__all__"   #字段，如果是__all__,就是表示列出所有的字段
+    exclude = None     #排除的字段
+    labels = None      #提示信息
+    help_texts = None    #帮助提示信息
+    widgets = None     #自定义插件
+    error_messages = None  #自定义错误信息
 class AddAssetView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'add_asset.html')
+        if request.method=='GET':
+            asset_list=AssetList()
+            return render(request, 'add_asset.html',{'asset_list':asset_list})
+        else:
+            asset_list=AssetList(request.POST)
+            if asset_list.is_valid():
+                asset_list.save()
+            return render(request, 'add_asset.html', {'asset_list': asset_list})
+
+
+
+
